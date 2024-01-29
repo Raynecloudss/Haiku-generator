d22939e553a727f8e7c45c429c387d7d80b58baf
Raynecloudss <rayne.a.fall@gmail.com>
2024-01-29 11:27:49 -0500

    Styling and AI added

--- src/javascript.js
+++ src/javascript.js
@@ -1,12 +1,25 @@
-function generateHaiku(event) {
-  event.preventDefault();
-  let haikuElement = document.querySelector("#haiku-lines");
+function displayHaiku(response) {
   new Typewriter("#haiku-lines", {
-    strings: "Consider me",
+    strings: response.data.answer,
     autoStart: true,
     delay: 85,
     cursor: null,
   });
+}
+
+function generateHaiku(event) {
+  event.preventDefault();
+
+  let topicInput = document.querySelector("#topic");
+  let apiKey = "a38c4204at848f00bec4c7fabo002e83";
+  let prompt = `User instructions: Generate a famous haiku about ${topicInput.value}`;
+  let context =
+    "You are a haiku expert. Your missions is to generate a 3 line haiku in basic HTML using <em> and <br /> without a title. Make sure to follow user instructions. Make sure sign the haiku 'SheCodes AI' at the bottom, and not the top using <strong>";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayHaiku);
+
+  let haikuElement = document.querySelector("#haiku-lines");
 }
 
 let poemFormElement = document.querySelector("#haiku-generator");
