ea0c9b3919026ae177a2612063160b6059c5d119
Raynecloudss <rayne.a.fall@gmail.com>
2024-01-30 21:28:20 -0500

    classes added, ux improved

--- src/javascript.js
+++ src/javascript.js
@@ -14,8 +14,16 @@ function generateHaiku(event) {
   let apiKey = "a38c4204at848f00bec4c7fabo002e83";
   let prompt = `User instructions: Generate a famous haiku about ${topicInput.value}`;
   let context =
-    "You are a haiku expert. Your missions is to generate a 3 line haiku in basic HTML using <em> and <br /> without a title. Make sure to follow user instructions. Make sure sign the haiku 'SheCodes AI' at the bottom, and not the top using <strong>";
+    "You are a haiku expert. Your missions is to generate a 3 line haiku in basic HTML using <em> and <br /> for the entire haiku. Create the haiku without a title. Make sure to follow user instructions. Make sure to sign the haiku 'SheCodes AI' at the bottom, and not the top using <strong> Do not alter the color of the text.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let generatingHaikuElement = document.querySelector("#haiku-lines");
+  new Typewriter("#haiku-lines", {
+    strings: `please hold...`,
+    autoStart: true,
+    delay: 50,
+    cursor: null,
+  });
 
   axios.get(apiUrl).then(displayHaiku);
 
